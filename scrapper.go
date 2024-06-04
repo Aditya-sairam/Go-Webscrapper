1be89672702ffa44b125d3e87dcb9b6e6974b5b3
Aditya Sairam Govindan <37501823+Aditya-sairam@users.noreply.github.com>
2024-06-04 10:08:22 -0400

    Adding the postgres database connect functionality

--- scrapper.go
+++ scrapper.go
@@ -1,7 +1,9 @@
-// This file contains the necessery code for the First Tutorial
+// Updated code for second tutorial.
 package main
 
 import (
+	//"database/sql"
+	//"encoding/json"
 
 	//"os"
 	"fmt"
@@ -9,14 +11,28 @@ import (
 	"time"
 
 	"github.com/gocolly/colly"
+	"github.com/jmoiron/sqlx"
+	_ "github.com/lib/pq"
 )
 
-// Hello there!
 type item struct {
 	Name         string `db:"name"`
 	Price        string `db:"price"`
 	Review       string `db:"review"`
 	DateScrapped string `db:"datescrapped"`
+}
+
+func databaseConnect(items item) {
+	db, err := sqlx.Connect("postgres", "user=postgres dbname=postgres password=your_password sslmode=disable")
+	fmt.Println(err)
+
+	query := `INSERT INTO GameDetails(name, price, review, date)
+	      VALUES(:name, :price, :review, :datescrapped)`
+	_, err = db.NamedExec(query, items)
+
+	if err != nil {
+		fmt.Println(err)
+	}
 }
 
 func extractSubstring(input string) string {
@@ -37,9 +53,7 @@ func webScrap() {
 			h.ForEach("div.col.search_reviewscore.responsive_secondrow", func(_ int, h *colly.HTMLElement) {
 				h.ForEach("span.search_review_summary.positive", func(_ int, h *colly.HTMLElement) {
 					linkStr := h.Attr("data-tooltip-html")
-
 					out := extractSubstring(linkStr)
-					// fmt.Println(out)
 					if len(out) > 0 {
 						review := out
 						item.Review = review
@@ -63,7 +77,6 @@ func webScrap() {
 				})
 			})
 			h.ForEach("span.title", func(_ int, h *colly.HTMLElement) {
-
 				name := h.Text
 				item.Name = name
 			})
@@ -79,6 +92,9 @@ func webScrap() {
 			items = append(items, item)
 		})
 		fmt.Println(items)
+		for i := 1; i < len(items); i++ {
+			databaseConnect(items[i])
+		}
 	})
 
 	c.Visit("https://store.steampowered.com/search/?filter=topsellers")
